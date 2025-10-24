d7b4be70dfe95a4b76d0baa1799e474e7e7cce6b
Alexandre Moretti <alemoretti10@gmail.com>
2025-10-24 10:33:58 -0300

    Create header and active links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
-import { Button } from "@/components/ui/button";
-import { Header } from "@/components/header/header";
+import { Header } from '@/components/header';
+import { Geist, Geist_Mono } from 'next/font/google';
+
+const geistSans = Geist({
+  subsets: ['latin'],
+  variable: '--font-geist-sans',
+});
+
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+});
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-zinc-50">
-      <div className="min-h-screen min-w-full bg-slate-500 p-8">
-        <Header />
-        <div className="mt-4 text-white">
-          <h1 className="text-2xl font-bold">Hello World!</h1>
-        </div>
-      </div>
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+      <Header />
     </div>
   );
 }
