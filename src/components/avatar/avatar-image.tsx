d7f9c24deafa55704953634c05cfdfd11e3c974f
Alexandre Moretti <alemoretti10@gmail.com>
2025-11-03 23:59:02 -0300

    Create avatar component

---
+++ src/components/avatar/avatar-image.tsx
@@ -0,0 +1,15 @@
+import Image, { ImageProps } from "next/image"
+
+type AvatarImageProps = ImageProps
+
+export const AvatarImage = ({ src, alt, width, height, ...rest }: AvatarImageProps) => {
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      {...rest}
+    />
+  );
+};
