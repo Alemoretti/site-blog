9d292d688415573765c83ded2df9eca9c69255bd
Alexandre Moretti <alemoretti10@gmail.com>
2025-11-15 10:37:14 -0300

    Layout improvements

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter, PT_Sans_Caption } from 'next/font/google';
 import { Footer } from '@/components/layout/footer';
 import { Header } from '@/components/layout/header';
+import { CallToAction } from '@/templates/landing-page/sections/call-to-action';
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -24,6 +25,7 @@ export const Layout = ({ children }: LayoutProps) => {
     >
       <Header />
       <main className="flex-1 flex flex-col mb-12">{children}</main>
+      <CallToAction />
       <Footer />
     </div>
   );
